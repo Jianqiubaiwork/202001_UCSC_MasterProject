7f508e5141431318a50de7cc837c2a5ad8e9e13e
Jianqiu Bai <jbai14@ucsc.edu>
2020-05-12 14:46:28 -0700

    all

--- apps/survey/urls.py
+++ apps/survey/urls.py
@@ -5,6 +5,6 @@ from .views import *
 app_name = 'survey'
 urlpatterns = [
     path('', OverView.as_view(), name='overview'),
-    path('survey/bail_judgement/', BailJudgementOverView.as_view(), name='bail_judgement_overview'),
-    path('survey/bail_judgement/<int:id>/', BailJudgementDetailView.as_view(), name='bail_judgement_detail'),
+    path('bail_judgement/', BailJudgementOverView.as_view(), name='bail_judgement_overview'),
+    path('bail_judgement/<int:id>/', BailJudgementDetailView.as_view(), name='bail_judgement_detail'),
 ]

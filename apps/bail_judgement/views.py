79d5a15bb82ce55a18b456368ad165290244e190
jianqiu1.bai <jianqiu1.bai@bytedance.com>
2020-05-23 17:22:49 -0700

    -a

--- apps/bail_judgement/views.py
+++ apps/bail_judgement/views.py
@@ -33,14 +33,27 @@ class SurveyView(TemplateView):
 
     def get(self, request):
         rand_objs = random.choices(CompasModel.objects.all(), k=10)
+        #rand_model_indicies = random.choices([1, 2, 3], k=)
+        all_trained_models_num = TrainedModel.objects.all().count()
         for rand_obj in rand_objs:
             rand_obj_prediction = CompasPredictionModel.objects.get(id=rand_obj.id)
-            rand_obj.prediction = rand_obj_prediction
+
+            rand_trained_models_indicies = random.sample(range(1, all_trained_models_num), 2)
+            rand_obj_prediction.one = None
+            rand_obj_prediction.two = None
+            rand_obj_prediction.three = None
+            if 1 in rand_trained_models_indicies:
+                rand_obj_prediction.one = rand_obj_prediction.predictor1_prob
+            elif 2 in rand_trained_models_indicies:
+                rand_obj_prediction.two = rand_obj_prediction.predictor2_prob
+            elif 3 in rand_trained_models_indicies:
+                rand_obj_prediction.three = rand_obj_prediction.predictor3_prob
         paginator = Paginator(rand_objs, 1)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         context = {
             'page_obj' : page_obj,
-            'page_number' : page_number
+            'page_number' : page_number,
+            'rand_trained_models_indicies' : rand_trained_models_indicies,
         }
         return render(request, self.template_name, context)

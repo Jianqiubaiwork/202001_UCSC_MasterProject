fbed6e8d3f000801c65ffbde712b2049a59266e2
jianqiu1.bai <jianqiu1.bai@bytedance.com>
2020-06-03 08:48:18 -0700

    -a

--- apps/loan_allocation/models.py
+++ apps/loan_allocation/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class HumanModel(models.Model):
+    first_name = models.CharField(null=True, max_length=100)
+    last_name = models.CharField(null=True, max_length=100)
+    gender = models.CharField(null=True, max_length=10)
+    job = models.CharField(null=True, max_length=100)
+    job_location = models.CharField(null=True, max_length=100)
+    age = models.IntegerField(null=True)
+    length_service = models.IntegerField(null=True)
+
